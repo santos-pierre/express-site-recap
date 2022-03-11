e171ffd1962987b58f6fd81a2550b214c72d6466
Santos Pierre <santospierre1305@gmail.com>
2022-03-11 16:00:58 +0100

    WIP - Contact Validation

--- src/controllers/ContactController.js
+++ src/controllers/ContactController.js
@@ -1,21 +1,32 @@
 const { Request, Response } = require('express');
+const { ContactFormValidationSchema } = require('../validator/ContactFormValidation');
 
-const { cryptos } = require('./../data/content_site.json');
 /**
  *
  * @param {Request} req
  * @param {Response} res
  */
 const index = (req, res) => {
-    res.render('pages/contact/index', { title: 'Contact' });
+    return res.render('pages/contact/index', { title: 'Contact' });
 };
 /**
  *
  * @param {Request} req
  * @param {Response} res
  */
-const sendForm = (req, res) => {
-    res.render('pages/contact/success', { title: 'Sucess', ...req.body });
+const sendForm = async (req, res) => {
+    console.log(req.body);
+    try {
+        await ContactFormValidationSchema.isValid(req.body);
+        res.json(req.body);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(422);
+    }
+
+    // if (ContactFormValidationSchema.isValid()) {
+    // }
+    // res.render('pages/contact/success', { title: 'Message Sent', ...req.body });
 };
 
 const ContactController = { index, sendForm };
---
+++ src/validator/ContactFormValidation.js
@@ -0,0 +1,11 @@
+const yup = require('yup');
+
+const ContactFormValidationSchema = yup.object().shape({
+    pseudo: yup.string().required(),
+    email: yup.string().email().required(),
+    first_name: yup.string(),
+    last_name: yup.string,
+    message: yup.string(),
+});
+
+module.exports = { ContactFormValidationSchema };
